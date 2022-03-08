8bb08dc25f4cb3fe70cc3a70ca4459902a09ffb0
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2022-03-08 22:26:24 +0700

    fix: import const

--- general.js
+++ general.js
@@ -1,5 +1,20 @@
-const { title } = require( '.' );
+const { danger } = require( 'danger' );
 
+// Github
+const { git, github } = danger;
+const {
+  modified_files,
+} = git;
+const {
+  pr, thisPR, api, issue,
+} = github;
+const { issues } = api;
+const {
+  body, title, changed_files, user, additions, deletions, assignees,
+} = pr;
+const { owner, repo, number } = thisPR;
+
+// Details
 const titleUpper = title.toUpperCase();
 const isWIP = titleUpper.includes(labels.wip.toString().toUpperCase());
 const details = {
@@ -21,5 +36,11 @@ const regex = {
 };
 
 module.exports = {
+  danger, git, github,
+  modified_files,
+  pr, thisPR, api, issue,
+  issues,
+  body, title, changed_files, user, additions, deletions, assignees,
+  owner, repo, number,
   isWIP, details, regex
 }
--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-const { details, isWIP, regex } = require( './general' );
+const _ = require( './general' );
 const { failures } = require( './message' );
-
-// Github
-const { git, github } = danger;
-const {
-  modified_files,
-} = git;
-const {
-  pr, thisPR, api, issue,
-} = github;
-const { issues } = api;
-const {
-  body, title, changed_files, user, additions, deletions, assignees,
-} = pr;
-const { owner, repo, number } = thisPR;
 
 // Function declaration and calling
 function checkPRChanges() {
-  const hasTooMuchFilesChanged = changed_files > details.max.changedFiles;
-  const hasTooMuchCodesChanged = additions + deletions > details.max.changedCodes;
+  const hasTooMuchFilesChanged = _.changed_files > details.max.changedFiles;
+  const hasTooMuchCodesChanged = _.additions + _.deletions > details.max.changedCodes;
 
   if (hasTooMuchFilesChanged || hasTooMuchCodesChanged) {
     fail(failures.tooMuchChanges());
@@ -26,54 +12,53 @@ function checkPRChanges() {
 }
 
 module.exports = {
-  title,
   prTitle() { // Force author to follow title rule
-    const isTitleSHort = title.match(regex.shortTitle);
+    const isTitleSHort = _.title.match(regex.shortTitle);
 
     if (isWIP) warn(warnings.wip);
     if (!isTitleSHort) fail(failures.tooShortTitle);
   },
   prDesc() { // Force author to follow description rule
-    const hasMinDesc = body.length < details.min.desc;
+    const hasMinDesc = _.body.length < details.min.desc;
 
     if (hasMinDesc) {
       warn(warnings.minDesc);
     }
   },
   prAssignees() { // Force author to assign assignee
-    const isEmptyAssignee = assignees.length === 0;
+    const isEmptyAssignee = _.assignees.length === 0;
     const payload = {
-      owner, repo, issue_number: number, assignees: user.login,
+      owner: _.owner, repo: _.repo, issue_number: _.number, assignees: _.user.login,
     };
 
-    if (isEmptyAssignee) issues.addAssignees(payload);
+    if (isEmptyAssignee) _.issues.addAssignees(payload);
   },
   prLabels() { // Force author to add label
-    const isEmptyLabel = (isWIP && issue.labels.length === 1)
-                          || (!isWIP && issue.labels.length === 0);
+    const isEmptyLabel = (isWIP && _.issue.labels.length === 1)
+                          || (!isWIP && _.issue.labels.length === 0);
     const payloadLabelAdd = {
-      owner, repo, issue_number: number, labels: labels.working_in_progress,
+      owner: _.owner, repo: _.repo, issue_number: _.number, labels: labels.working_in_progress,
     };
-    const payloadLabelList = { owner, repo, issue_number: number };
+    const payloadLabelList = { owner: _.owner, repo: _.repo, issue_number: _.number };
     const payloadLabelRemove = {
-      owner, repo, issue_number: number, name: labels.working_in_progress,
+      owner: _.owner, repo: _.repo, issue_number: _.number, name: labels.working_in_progress,
     };
 
     if (isEmptyLabel) fail(failures.noLabel);
     if (isWIP) {
-      issues.addLabels(payloadLabelAdd);
+      _.issues.addLabels(payloadLabelAdd);
     } else {
-      issues.listLabelsOnIssue(payloadLabelList).then((response) => {
+      _.issues.listLabelsOnIssue(payloadLabelList).then((response) => {
         response.data.forEach((arr) => {
           if (JSON.stringify(arr.name).includes(labels.working_in_progress)) {
-            issues.removeLabel(payloadLabelRemove);
+            _.issues.removeLabel(payloadLabelRemove);
           }
         });
       });
     }
   },
   prChangesCount() { // Force author to create small changes
-    const isFileModified = details.exclude.filter((e) => modified_files.includes(e)).length > 0;
+    const isFileModified = details.exclude.filter((e) => _.modified_files.includes(e)).length > 0;
 
     if (isFileModified) {
       fail(failures.excludeFilesChanged);
