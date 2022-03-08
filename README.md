ac0126d84de36b8ce994381556f7f919b89fe0bf
Ferawati Hartanti Pratiwi <ferawati.10109266@gmail.com>
2022-03-08 20:58:59 +0700

    feat: init danger rules

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # danger-js-github-template
+
+### How to install
+HTTP version - `npm i git+https://github.com/mpermperpisang/danger-js-github-template.git#master --save-optional`<br/>
+SSH version - `npm i git+ssh://git@github.com:mpermperpisang/danger-js-github-template.git#master --save-optional`
+
+### How to setup
+1. Put this code in dangerfile.js of <b>your repository</b>.
+```
+// dangerfile.js
+
+import {
+  prTitle, prDesc, prAssignees, prLabels, prChangesCount, prCommits,
+} from 'danger-js-github-template ';
+
+prTitle();
+prDesc();
+prAssignees();
+prLabels();
+prChangesCount();
+prCommits();
+```
+
+2. <b>In local env</b>, we must export danger token in .bash_profile (or any profile you usually used)<br/>
+`export DANGER_GITHUB_API_TOKEN=<your token here>`
+
+### Contact
+Email - mpermperpisang@gmail.com<br/>
+Twitter - mpermperpisang
---
+++ index.js
@@ -0,0 +1,84 @@
+const { details, isWIP, regex } = require( './general' );
+const { failures } = require( './message' );
+
+const { git, github } = danger;
+const {
+  modified_files,
+} = git;
+const {
+  pr, thisPR, api, issue,
+} = github;
+const { issues } = api;
+const {
+  body, title, changed_files, user, additions, deletions, assignees,
+} = pr;
+const { owner, repo, number } = thisPR;
+
+function checkPRChanges() {
+  const hasTooMuchFilesChanged = changed_files > details.max.changedFiles;
+  const hasTooMuchCodesChanged = additions + deletions > details.max.changedCodes;
+
+  if (hasTooMuchFilesChanged || hasTooMuchCodesChanged) {
+    fail(failures.tooMuchChanges());
+  }
+}
+
+module.exports = {
+  prTitle() { // Force author to follow title rule
+    const isTitleSHort = title.match(regex.shortTitle);
+
+    if (isWIP) warn(warnings.wip);
+    if (!isTitleSHort) fail(failures.tooShortTitle);
+  },
+  prDesc() { // Force author to follow description rule
+    const hasMinDesc = body.length < details.min.desc;
+
+    if (hasMinDesc) {
+      warn(warnings.minDesc);
+    }
+  },
+  prAssignees() { // Force author to assign assignee
+    const isEmptyAssignee = assignees.length === 0;
+    const payload = {
+      owner, repo, issue_number: number, assignees: user.login,
+    };
+
+    if (isEmptyAssignee) issues.addAssignees(payload);
+  },
+  prLabels() { // Force author to add label
+    const isEmptyLabel = (isWIP && issue.labels.length === 1)
+                          || (!isWIP && issue.labels.length === 0);
+    const payloadLabelAdd = {
+      owner, repo, issue_number: number, labels: labels.working_in_progress,
+    };
+    const payloadLabelList = { owner, repo, issue_number: number };
+    const payloadLabelRemove = {
+      owner, repo, issue_number: number, name: labels.working_in_progress,
+    };
+
+    if (isEmptyLabel) fail(failures.noLabel);
+    if (isWIP) {
+      issues.addLabels(payloadLabelAdd);
+    } else {
+      issues.listLabelsOnIssue(payloadLabelList).then((response) => {
+        response.data.forEach((arr) => {
+          if (JSON.stringify(arr.name).includes(labels.working_in_progress)) {
+            issues.removeLabel(payloadLabelRemove);
+          }
+        });
+      });
+    }
+  },
+  prChangesCount() { // Force author to create small changes
+    const isFileModified = details.exclude.filter((e) => modified_files.includes(e)).length > 0;
+
+    if (isFileModified) {
+      fail(failures.excludeFilesChanged);
+    } else {
+      checkPRChanges();
+    }
+  },
+  prCommits() { // Force author to follow commit message format
+    if (!lastCommit.match(regex.commitPrefix)) warn(warnings.lastCommit(lastCommit));
+  },
+}
